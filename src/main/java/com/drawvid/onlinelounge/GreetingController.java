531fedf35e9d833145fb7bd14dec2b8910c87228
dldurks <dldurks@gmail.com>
2021-01-02 16:50:42 -0600

    empty websocket server

---
+++ src/main/java/com/drawvid/onlinelounge/GreetingController.java
@@ -0,0 +1,18 @@
+package com.drawvid.onlinelounge;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(com.drawvid.onlinelounge.HelloMessage message) throws Exception {
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+
+}
